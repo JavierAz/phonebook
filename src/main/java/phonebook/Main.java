564c2985832ca3feba71c1a9384785fb72efedbb
Javier Azanza <azzanza09@gmail.com>
2020-03-30 12:56:15 -0600

    Conexion a la base de datos

--- src/main/java/phonebook/Main.java
+++ src/main/java/phonebook/Main.java
@@ -5,6 +5,11 @@
  */
 package phonebook;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import phonebook.db.Conection;
+
 /**
  *
  * @author javier
@@ -14,8 +19,20 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        String servidor = "localhost";
+        String database = "escuela";
+        String usuario = "escuela";
+        String password = "P@ssw0rd";
+        Conection conn =  new Conection(servidor, database, usuario, password);
+        
+        PreparedStatement st = conn.getConexion().prepareStatement("SELECT * FROM estudiantes");
+        ResultSet rs = st.executeQuery();
+        while(rs.next()){
+            System.out.println("id = " + rs.getLong("id"));
+            System.out.println("nombre = " + rs.getString("nombre"));   
+        }
     }
     
 }
